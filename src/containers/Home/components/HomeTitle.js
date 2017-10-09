2141b0c9b56bd7990997422ca8dda15b75996c0d
ChaoTzuJung <s110319022@stu.ntue.edu.tw>
2017-10-10 02:26:32 +0800

    initial commit

---
+++ src/containers/Home/components/HomeTitle.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class HomeTitle extends Component {
+  render() {
+    return (
+      <div className="section" id="homeTitle">
+        <h1>Hello, We are ReactMaker!</h1>
+      </div>
+    );
+  }
+}
