d69722c3f7f1894306b32a0852733541008f15e0
ChaoTzuJung <s110319022@stu.ntue.edu.tw>
2017-11-16 17:24:50 +0800

    fix collapsible

--- src/containers/Home/components/Collapsible/Collapsible.js
+++ src/containers/Home/components/Collapsible/Collapsible.js
@@ -2,18 +2,58 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Collapsible extends Component {
-
+  componentDidMount() {
+    window.$(document).ready(() => {
+      $(document).ready(() => {
+        $('.collapsible').collapsible();
+      });
+    });
+  }
   render() {
     return (
       <div>
         <ul class="collapsible" data-collapsible="accordion">
           <li>
             <div class="collapsible-header"><i class="material-icons">filter_drama</i>前端工程師 x 1</div>
-            <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+            <div class="collapsible-body">
+              <span>
+                <h5>工作內容</h5><br/>
+                <ul>
+                  <li>與設計師及後端工程師合作進行網頁的開發與維護</li>
+                </ul>
+                <ul>
+                  <li>使用過 JS Framework 撰寫網站 ex: React, Vue, Angular</li>
+                  <li>Style 語言為 Sass + Styled Component 並且使用 Bootstrap</li>
+                  <li>Build system 使用 webpack</li>
+                </ul>
+                <h5>勝任這份工作的基本條件</h5><br/>
+                <ul>
+                  <li>使用過 JS Framework 撰寫網站 ex: React, Vue, Angular</li>
+                  <li>Style 語言為 Sass + Styled Component 並且使用 Bootstrap</li>
+                  <li>Build system 使用 webpack</li>
+                </ul>
+              </span>
+            </div>
           </li>
           <li>
             <div class="collapsible-header"><i class="material-icons">place</i>後端工程師 x 1</div>
-            <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+            <div class="collapsible-body">
+              <span>
+                <h5>工作內容</h5><br/>
+                <ul>
+                  <li>網站 API 開發與維護</li>
+                  <li>第三方服務（郵件、客服等）系統整合</li>
+                  <li>協助伺服器維護與管理</li>
+                </ul>
+                <h5>勝任這份工作的基本條件</h5><br/>
+                <ul>
+                  <li>熟悉 NodeJS (ExpressJS)</li>
+                  <li>熟悉 MongoDB (Mongoose)</li>
+                  <li>曾經有開發 Restful API 的經驗</li>
+                  <li>熟悉 Git</li>
+                </ul>
+              </span>
+            </div>
           </li>
           <li>
             <div class="collapsible-header"><i class="material-icons">whatshot</i>社群經營與行銷實習 x 1~3</div>
